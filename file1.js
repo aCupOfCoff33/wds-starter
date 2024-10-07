b01cc0cb0d3c8c8b84f4367f5989252db2eb5cc3
Aaryan <aaryanj@outlook.com>
2024-10-07 19:10:16 -0400

    Update file1.js

--- file1.js
+++ file1.js
@@ -4,12 +4,12 @@
  * just uncomment them to implement them, if you don't mind.
  */
 
-function feature1() {
-  console.log("FEATURE 1 IS SO COOL");
-  return new Promise(() => "to be cool");
-}
+// function feature1() {
+//  console.log("FEATURE 1 IS SO COOL");
+//  return new Promise(() => "to be cool");
+// }
 
-function feature2() {
-  console.log("FEATURE 1 IS KINDA A LIAR");
-  return new Promise((resolve, reject) => reject("to be cool"));
-}
+// function feature2() {
+//   console.log("FEATURE 1 IS KINDA A LIAR");
+//   return new Promise((resolve, reject) => reject("to be cool"));
+// }
